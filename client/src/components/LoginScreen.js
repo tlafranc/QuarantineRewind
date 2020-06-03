a28d203a1845283770cecc1800654ef6148b6eb6
Thomas Lafrance <tlafranc@ualberta.ca>
2020-06-03 17:13:39 -0600

    Fixed login URL for production and development in client, removed .env file from repo.

--- client/src/components/LoginScreen.js
+++ client/src/components/LoginScreen.js
@@ -1,7 +1,9 @@
 // Import libraries
 import React from 'react';
 
-const login_uri = 'http://localhost:8888/login';
+const login_uri = process.env.NODE_ENV == "PRODUCTION" 
+    ? 'https://quarantine-rewind.herokuapp.com/login'
+    : 'http://localhost:8888/login';
 
 class LoginScreen extends React.Component {
     constructor(props) {
