f003981f585c15a0a676c418ff22eb8ce03c3fae
Thomas Lafrance <tlafranc@ualberta.ca>
2020-10-27 21:34:39 -0600

    Fixed no image positioning.

--- client/src/components/ThreeImageLayout.js
+++ client/src/components/ThreeImageLayout.js
@@ -18,26 +18,50 @@ class ThreeImageLayout extends React.Component {
 
         return (
             <div className="ThreeImageLayout" style={{height: `${width}px`}}>
-                {!images[2] ? null : 
-                    <img className="image" src={_.get(images[2],'url') || SpotifyIcon} alt={`${images[2].alt}`} width={`${width}px`} height={`${width}px`} 
-                        style={{
-                            transform: `translate(${width * 2}px, ${verticalShift}px)`
-                        }}
-                    />
+                {!images[2] 
+                    ?  
+                        <img className="image" src={SpotifyIcon} alt={'Spotify Icon'} width={`${width}px`} height={`${width}px`} 
+                            style={{
+                                transform: `translate(${width * 2}px, ${verticalShift}px)`,
+                                opacity: 0
+                            }}
+                        />
+                    : 
+                        <img className="image" src={_.get(images[2],'url') || SpotifyIcon} alt={`${images[2].alt}`} width={`${width}px`} height={`${width}px`} 
+                            style={{
+                                transform: `translate(${width * 2}px, ${verticalShift}px)`
+                            }}
+                        />
                 }
-                {!images[1] ? null :
-                    <img className="image" src={_.get(images[1],'url') || SpotifyIcon} alt={`${images[1].alt}`} width={`${width}px`} height={`${width}px`} 
-                        style={{
-                            transform: `translate(${width * 1 / 5}px, 0)`
-                        }}
-                    />
+                {!images[1] 
+                    ? 
+                        <img className="image" src={SpotifyIcon} alt={'Spotify Icon'} width={`${width}px`} height={`${width}px`} 
+                            style={{
+                                transform: `translate(${width * 1 / 5}px, 0)`,
+                                opacity: 0
+                            }}
+                        />
+                    :
+                        <img className="image" src={_.get(images[1],'url') || SpotifyIcon} alt={`${images[1].alt}`} width={`${width}px`} height={`${width}px`} 
+                            style={{
+                                transform: `translate(${width * 1 / 5}px, 0)`
+                            }}
+                        />
                 }
-                {!images[0] ? null :
-                    <img className="image" src={_.get(images[0],'url') || SpotifyIcon} alt={`${images[0].alt}`} width={`${width}px`} height={`${width}px`} 
-                        style={{
-                            transform: `translate(${-width * 8 / 5}px, ${-1 * verticalShift}px)`
-                        }}
-                    />
+                {!images[0] 
+                    ? 
+                        <img className="image" src={SpotifyIcon} alt={'Spotify Icon'} width={`${width}px`} height={`${width}px`} 
+                            style={{
+                                transform: `translate(${-width * 8 / 5}px, ${-1 * verticalShift}px)`,
+                                opacity: 0
+                            }}
+                        /> 
+                    :
+                        <img className="image" src={_.get(images[0],'url') || SpotifyIcon} alt={`${images[0].alt}`} width={`${width}px`} height={`${width}px`} 
+                            style={{
+                                transform: `translate(${-width * 8 / 5}px, ${-1 * verticalShift}px)`
+                            }}
+                        />
                 }
             </div>
         );
